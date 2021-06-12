3aec8c960adc24f2f7fc7db8816a659f57508cca
Phil Kulak <phil.kulak@vevo.com>
2021-06-12 15:41:27 -0700

    Proper configuration.

--- src/main/kotlin/com/pkulak/memo/MemoModule.kt
+++ src/main/kotlin/com/pkulak/memo/MemoModule.kt
@@ -1,16 +1,18 @@
 package com.pkulak.memo
 
 import com.pkulak.memo.storage.BlockStorage
+import com.typesafe.config.ConfigFactory
 import org.jetbrains.exposed.sql.Database
 import org.koin.dsl.module
 
 val memoModule = module {
-    // TODO: some configuration system
+    val config = ConfigFactory.load()!!
+
     single {
-        Database.connect("jdbc:postgresql://localhost/memo",
-            driver = "org.postgresql.Driver",
-            user = "memo",
-            password = "memo"
+        Database.connect(config.getString("database.url"),
+            driver = config.getString("database.driver"),
+            user = config.getString("database.username"),
+            password = config.getString("database.password")
         )
     }
 
